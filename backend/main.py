24b60f9a4427e52b91adb1d7cac436be6d3488aa
Shyam Kumar <yourgithubemail@gmail.com>
2026-05-19 19:55:55 +0530

    Initialized FastAPI backend successfully

---
+++ backend/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {
+        "message": "Backend Running Successfully"
+    }
+
+@app.get("/health")
+def health():
+    return {
+        "status": "healthy"
+    }
